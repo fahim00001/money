cf8f29376d593104fc603f9bf6193b23db7edd98
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-02-17 11:33:17 +0600

    css finished

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.getElementById('calculator-button').addEventListener('click',function(){
+   const incomeInput = document.getElementById('income-input');
+   ;
+   const incomeAmount = incomeInput.value;
+   console.log(incomeAmount);
+   
+   
+   
+});
