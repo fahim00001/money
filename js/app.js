0819b691e1f9d1ec4f04ec08a62421f15dd3e79c
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-02-17 12:23:21 +0600

    fucntiondelaration

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
+///function declaration for input values
+function getinputid(inputId){
+    const getmoneyInput = document.getElementById(inputId +"-input");
+    const inputValues = getmoneyInput.value;
+    return inputValues;
+}
+
 document.getElementById('calculator-button').addEventListener('click',function(){
    const incomeInput = document.getElementById('income-input');
    ;
    const incomeAmount = incomeInput.value;
    console.log(incomeAmount);
    
-   
+   const foodInput = document.getElementById('food-input');
+   const foodExpenseAmount = foodInput.value;
+   console.log(foodExpenseAmount);
+
+   const rentInput = document.getElementById('rent-input');
+   const rentExpenseAmount = rentInput.value;
+   console.log(rentExpenseAmount);
+
+   const colthesInput = document.getElementById('clothes-input');
+   const colthExpenseAmount = colthesInput.value;
+   console.log(colthExpenseAmount);
    
 });
