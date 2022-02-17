e8139005c1c5b2fd1f80a2de0885a56ee9f06013
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-02-17 16:11:12 +0600

    complete without error handaling

--- js/app.js
+++ js/app.js
@@ -1,42 +1,53 @@
 ///function declaration for input values
-function getinputid(inputId){
+function getinputId(inputId){
     const getmoneyInput = document.getElementById(inputId +"-input");
     const inputAmountText = getmoneyInput.value;
     const amountValue = parseFloat(inputAmountText);
     return amountValue;
 }
+// function declaration for text values
 
+function getTextFieldId(textId,textFieldName){
+    const textField =document.getElementById(textId);
+    textField.innerText = textFieldName;
+    const textFieldValue =  textField.innerText;
+    return textFieldValue;
+}
 
 document.getElementById('calculator-button').addEventListener('click',function(){
     //income amount
-   const incomeAmount = getinputid('income');
+   const incomeAmount = getinputId('income');
     //food expenes
-   const foodCostAmount= getinputid('food');
+   const foodCostAmount= getinputId('food');
    //rent expenes
-   const rentCostAmount = getinputid('rent');
+   const rentCostAmount = getinputId('rent');
    //colthes expenes
-   const colthesCostAmount = getinputid('clothes');
+   const colthesCostAmount = getinputId('clothes');
 
-   ///////////////expenses calculation/////////////
-    const totalExpensesAmount = foodCostAmount + rentCostAmount +colthesCostAmount;
+     ///////////////expenses calculation/////////////
+     const totalExpensesAmount = foodCostAmount + rentCostAmount + colthesCostAmount;
+     
+     const totalExpenses = getTextFieldId('total-expence',totalExpensesAmount);
 
-    ///show total expence and balance 
+        //total balance caculation
+     const totalBalanceAmount = incomeAmount - totalExpensesAmount;
+
+     const totalBalance = getTextFieldId('total-balance',totalBalanceAmount);
+  
+       /////////////////savings  calculation////////
+
+     document.getElementById('saving-input-btn').addEventListener('click',function(){
+        const incomeAmount = getinputId('income');
+        const savingPercentageFeild = getinputId('saving');
+        
+        const savingAmount = incomeAmount * (savingPercentageFeild /100)
     
-   const totalExpenses =document.getElementById('total-expence');
-   totalExpenses.innerText = totalExpensesAmount;
-
-   const totalBalanceAmount = incomeAmount - totalExpensesAmount;
-
-   const totalBalance = document.getElementById('total-balance');
-   totalBalance.innerText = totalBalanceAmount;
-
-
+        const  savingAmountfield = getTextFieldId('saving-amount-text',savingAmount);
+    
+        const remaingBalanceAmount = totalBalanceAmount - savingAmount;
+    
+        const remaingBalancefield = getTextFieldId('remaining-balance-field',remaingBalanceAmount);
+    
+    });
 });
 
-document.getElementById('saving-input-btn').addEventListener('click',function(){
-    const incomeAmount = getinputid('income');
-    const savingPercentageFeild = getinputid('saving');
-    
-    const savingAmount = incomeAmount * (savingPercentageFeild /100)
-    console.log(savingAmount);
-})
